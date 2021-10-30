9875c530f7ee2fc0b3b85353b5ebc2917200b760
Anna Vasileva <anette.vasileva@gmail.com>
2021-10-30 23:18:31 +0300

    adds name removal

--- src/App.js
+++ src/App.js
@@ -6,13 +6,13 @@ import ContactList from "./components/ContactList";
 
 class App extends Component {
   state = {
-    contacts: [],
-    // contacts: [
-    //   { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-    //   { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-    //   { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-    //   { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-    // ],
+    // contacts: [],
+    contacts: [
+      { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+      { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+      { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+      { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+    ],
     filter: "",
   };
   formSubmitData = (data) => {
@@ -27,7 +27,12 @@ class App extends Component {
     this.setState({ filter: e.currentTarget.value });
   };
   checkNewName = (name) => {
-    return this.state.contacts.find((el) => el.name.includes(name));
+    return this.state.contacts.find((el) => el.name === name);
+  };
+  deleteContact = (id) => {
+    this.setState((prevState) => ({
+      contacts: prevState.contacts.filter((el) => el.id !== id),
+    }));
   };
 
   render() {
@@ -41,6 +46,7 @@ class App extends Component {
         <ContactList
           contacts={this.state.contacts}
           filter={this.state.filter}
+          onDelete={this.deleteContact}
         />
       </div>
     );
--- src/components/ContactList/ContactList.js
+++ src/components/ContactList/ContactList.js
@@ -3,7 +3,7 @@ import s from "./ContactList.module.css";
 
 class ContactList extends Component {
   render() {
-    const { contacts, filter } = this.props;
+    const { contacts, filter, onDelete } = this.props;
     const normalizeFilter = filter.toLowerCase();
     const visibleContacts = contacts.filter((el) =>
       el.name.toLowerCase().includes(normalizeFilter)
@@ -15,6 +15,13 @@ class ContactList extends Component {
           return (
             <li key={id} className={s.item}>
               {name}: {number}
+              <button
+                type="button"
+                onClick={() => onDelete(id)}
+                className={s.btn}
+              >
+                Delete
+              </button>
             </li>
           );
         })}
